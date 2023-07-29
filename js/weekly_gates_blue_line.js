32ce0b5f10ab84698d779c8777c2614f8b9a4119
Dayne <dayne.bloxham@gmail.com>
2023-07-29 16:05:57 -0400

    Add reset chart buttons, functionality of dropdown for Blue line

--- js/weekly_gates_blue_line.js
+++ js/weekly_gates_blue_line.js
@@ -1,112 +1,114 @@
 // Load the data from the CSV file
-d3.csv('https://raw.githubusercontent.com/dbloxham1/cs416-dataviz-final/main/data/gated_entries_by_week_route.csv').then(data => {
-    // Parse the date and convert gated_entries to number
-    const parseTime = d3.timeParse('%Y-%m-%d');
-    data = data.filter(d => d.route_short === 'Blue').map(d => {
-        d.firstDayOfWeek = parseTime(d.firstDayOfWeek);
-        d.gated_entries = +d.gated_entries;
-        return d;
-    });
+function initialChart() {
+    d3.csv('https://raw.githubusercontent.com/dbloxham1/cs416-dataviz-final/main/data/gated_entries_by_week_route.csv').then(data => {
+        // Parse the date and convert gated_entries to number
+        const parseTime = d3.timeParse('%Y-%m-%d');
+        data = data.filter(d => d.route_short === 'Blue').map(d => {
+            d.firstDayOfWeek = parseTime(d.firstDayOfWeek);
+            d.gated_entries = +d.gated_entries;
+            return d;
+        });
 
-    // Set the dimensions and margins of the graph
-    const margin = {top: 20, right: 75, bottom: 20, left: 75};
-    const width = 960 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
+        // Set the dimensions and margins of the graph
+        const margin = {top: 20, right: 75, bottom: 20, left: 75};
+        const width = 960 - margin.left - margin.right;
+        const height = 500 - margin.top - margin.bottom;
 
-    // Append the svg object to the body of the page
-    const svg = d3.select('#blue')
+        // Append the svg object to the body of the page
+        const svg = d3.select('#blue')
                   .append('svg')
                   .attr('width', width + margin.left + margin.right)
                   .attr('height', height + margin.top + margin.bottom)
                   .append('g')
                   .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-    // Define the scales
-    const x = d3.scaleTime()
+        // Define the scales
+        const x = d3.scaleTime()
                      .domain(d3.extent(data, d => d.firstDayOfWeek))
                      .range([0, width]);
-    const y = d3.scaleLinear()
+        const y = d3.scaleLinear()
                      .domain([0, d3.max(data, d => d.gated_entries)])
                      .range([height, 0]);
     
-    var tooltip = d3.select('#tooltip');
+        var tooltip = d3.select('#tooltip');
 
-    const line = d3.line()
-        .x(d => x(d.firstDayOfWeek))
-        .y(d => y(d.gated_entries));
+        const line = d3.line()
+            .x(d => x(d.firstDayOfWeek))
+            .y(d => y(d.gated_entries));
 
-    svg.append('path')
-        .style("stroke","blue")
-        .data([data])
-        .attr('class', 'line')
-        .attr('d', line);
+        svg.append('path')
+            .style("stroke","blue")
+            .data([data])
+            .attr('class', 'line')
+            .attr('d', line);
 
-    // Add the X Axis
-    svg.append('g')
-       .attr('transform', `translate(0, ${height})`)
-       .call(d3.axisBottom(x));
+        // Add the X Axis
+        svg.append('g')
+            .attr('transform', `translate(0, ${height})`)
+            .call(d3.axisBottom(x));
 
-    // Add the Y Axis
-    svg.append('g')
-       .call(d3.axisLeft(y));
+        // Add the Y Axis
+        svg.append('g')
+            .call(d3.axisLeft(y));
 
-    const timeFormat = d3.timeFormat('%Y-%m-%d');
+        const timeFormat = d3.timeFormat('%Y-%m-%d');
 
-    const annotations = [
-        {
-            note: {
-                title: 'Mar 9, 2020',
-                label: 'COVID Shutdown Begins'
-            },
-            data: {firstDayOfWeek: '2020-03-09', gated_entries: 251409},
-            dy: 100,
-            dx: 50,
-            subject: {
-                radius: 15
+        const annotations = [
+            {
+                note: {
+                    title: 'Mar 9, 2020',
+                    label: 'COVID Shutdown Begins'
+                },
+                data: {firstDayOfWeek: '2020-03-09', gated_entries: 251409},
+                dy: 100,
+                dx: 50,
+                subject: {
+                    radius: 15
+                }
+            }, {
+                note: {
+                    title: 'May 18, 2020',
+                    label: "Scheduled Improvements between Airport and Bowdoin"
+                },
+                data: {firstDayOfWeek: '2020-05-18', gated_entries: 2944},
+                dy: -30,
+                dx: 50,
+                subject: {
+                    radius: 15
+                }
+            }, {
+                note: {
+                    title: 'Apr 25, 2022',
+                    label: "Continued Improvements Between Airport and Bowdoin"
+                }, 
+                data: {firstDayOfWeek: '2022-04-25', gated_entries: 81115},
+                dy: 20,
+                dx: -50,
+                subject: {
+                    radius: 15
+                }
             }
-        }, {
-            note: {
-                title: 'May 18, 2020',
-                label: "Scheduled Improvements between Airport and Bowdoin"
-            },
-            data: {firstDayOfWeek: '2020-05-18', gated_entries: 2944},
-            dy: -30,
-            dx: 50,
-            subject: {
-                radius: 15
-            }
-        }, {
-            note: {
-                title: 'Apr 25, 2022',
-                label: "Continued Improvements Between Airport and Bowdoin"
-            }, 
-            data: {firstDayOfWeek: '2022-04-25', gated_entries: 81115},
-            dy: 20,
-            dx: -50,
-            subject: {
-                radius: 15
-            }
-        }
-    ];
+        ];
    
-    window.makeAnnotations = d3.annotation()
-        .annotations(annotations)
-        .type(d3.annotationCalloutCircle)
-        .accessors({
-            x: d => x(parseTime(d.firstDayOfWeek)),
-            y: d => y(d.gated_entries)
-        })
-        .accessorsInverse({
-            firstDayOfWeek: d => timeFormat(x.invert(d.x)),
-            gated_entries: d => y.invert(d.y)
-        })
+        window.makeAnnotations = d3.annotation()
+            .annotations(annotations)
+            .type(d3.annotationCalloutCircle)
+            .accessors({
+                x: d => x(parseTime(d.firstDayOfWeek)),
+                y: d => y(d.gated_entries)
+            })
+            .accessorsInverse({
+                firstDayOfWeek: d => timeFormat(x.invert(d.x)),
+                gated_entries: d => y.invert(d.y)
+            })
    
-    svg.append('g')
-        .attr('class','annotation-test')
-        .call(makeAnnotations)
-    ;
+        svg.append('g')
+            .attr('class','annotation-test')
+            .call(makeAnnotations)
+        ;
    
-    svg.selectAll('g.annotation-connector, g.annotation-note')
-        .classed('hidden',true)
-    ;
-});
+        svg.selectAll('g.annotation-connector, g.annotation-note')
+            .classed('hidden',true)
+        ;
+    });
+};
